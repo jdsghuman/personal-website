00bc8f81c24f457c447d8c8c96c978d7269ce62b
JD Ghuman <jagdeep.x.ghuman.-nd@disney.com>
2017-06-30 13:35:00 -0400

    Update subtitle and css format

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch'),
-browserSync = require('browser-sync').create();
+  watch = require('gulp-watch'),
+  browserSync = require('browser-sync').create();
 
 gulp.task('watch', function() {
 
-    browserSync.init({
-      notify: false,
+  browserSync.init({
+    notify: false,
       server: {
         baseDir: "app"
       }
