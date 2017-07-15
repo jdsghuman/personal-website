1e81436c06e52e216f7d1523d1d7670d7e9d6117
JD Ghuman <jdsghuman@gmail.com>
2017-07-15 15:33:32 -0400

    Add OnScroll class

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,13 @@
 import MobileMenu from './modules/MobileMenu';
+import OnScroll from './modules/OnScroll';
 import Email from './modules/Email';
 import Copyright from './modules/Copyright';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
 var email = new Email();
 var copyright = new Copyright();
+
+var elm = document.querySelectorAll('.portfolio-item');
+
+new OnScroll($('.portfolio-item'), '85%');
---
+++ app/assets/scripts/modules/OnScroll.js
@@ -0,0 +1,31 @@
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+import $ from 'jquery';
+
+class OnScroll {
+  constructor(elm, offset) {
+    this.itemsToReveal = elm;
+    this.offsetPercentage = offset;
+    this.hideInitially();
+    this.createWaypoints();
+  }
+
+  hideInitially() {
+    this.itemsToReveal.addClass('reveal-item');
+   }
+
+  createWaypoints() {
+    var that = this;
+    this.itemsToReveal.each(function() {
+      var currentItem = this;
+      new Waypoint({
+        element: currentItem,
+        handler: function() {
+          $(currentItem).addClass('reveal-item--is-visible');
+        },
+        offset: that.offsetPercentage
+      });
+    });
+  }
+}
+
+export default OnScroll;
