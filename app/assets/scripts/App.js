04812ec9392875485dbb445d1aa4005a9adc3cc2
JD Ghuman <jdsghuman@gmail.com>
2017-07-18 18:04:50 -0400

    Add smooth scroll functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,11 +3,12 @@ import OnScroll from './modules/OnScroll';
 import Email from './modules/Email';
 import Copyright from './modules/Copyright';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
+
+var elm = document.querySelectorAll('.portfolio-item');
 
 var mobileMenu = new MobileMenu();
 var email = new Email();
 var copyright = new Copyright();
-
-var elm = document.querySelectorAll('.portfolio-item');
-
 new OnScroll($('.portfolio-item'), '85%');
+var stickyHeader = new StickyHeader();
